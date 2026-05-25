10129e5e205798c88eceb1a59037367377de0324
Fuad Sharif <fuadsharif004@gmail.com>
2026-05-25 13:23:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,45 @@
 import random
-
 from game_data import data
+from art import logo, vs
 
-def option_a_i(info):
+def option_formate(option):
+    name= option["name"]
+    desc= option["description"]
+    country= option["country"]
+    return f"{name}, a {desc}, from {country}"
 
-    details= f"Compare A: {info['name']}, a {info['description']}, from {info['country']}"
-    return details
+def check_answer(user_guess,followers_a,followers_b):
+    if followers_a > followers_b:
+        return user_guess == "a"
+    else:
+        return user_guess == "b"
 
+print(logo)
+score=0
+play= True
+option_b= random.choice(data)
+while play:
+    option_a= option_b
+    option_b= random.choice(data)
+    if option_a==option_b:
+        option_b= random.choice(data)
 
-def option_b_i(info):
-    details= f"Against B: {info['name']}, a {info['description']}, from {info['country']}"
-    return details
+    print(f"Compare A: {option_formate(option_a)}")
+    print(vs)
+    print(f"Against B: {option_formate(option_b)}")
+    guess= input("Who has more followers? Type 'A' or 'B':").lower()
 
+    print("\n" * 20)
+    print(logo)
 
-def option_a_f(followers):
-    follower_count= followers["follower_count"]
-    return follower_count
+    f_a= option_a["follower_count"]
+    f_b= option_b["follower_count"]
+    is_correct= check_answer(guess,f_a,f_b)
 
-def option_b_f(followers):
-    follower_count= followers["follower_count"]
-    return follower_count
+    if is_correct:
+        score+=1
+        print(f"You're right! Current score: {score}")
 
-def compare(a,b):
-    
-
-
-
-
-
-
-
-
-list_item_a= random.choice(data)
-list_item_b= random.choice(data)
-f_a= option_a_f(list_item_a)
-f_b= option_b_f(list_item_b)
-
-print(option_a_i(list_item_a))
-print(option_b_i(list_item_b))
-
-
-
-
-
-
+    else:
+        print(f"Sorry, that's wrong. Final score: {score}")
+        play= False
